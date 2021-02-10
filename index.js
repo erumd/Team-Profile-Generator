57f0f8b1a2884914b94ed7d01d69db920497d482
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-09 23:11:28 -0600

    updated index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 
 const Intern = require("./lib/intern");
+
 const Manager = require("./lib/manager");
 
 const questions = require("./questions");
