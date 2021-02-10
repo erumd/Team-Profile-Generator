8bf3021ff0e081890c772e961987847fb020e962
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-09 22:56:03 -0600

    added intern questions

--- questions.js
+++ questions.js
@@ -1,74 +1,83 @@
-
 const role = [
-    {
-    type: 'list',
-    name: 'choice',
-    message: 'Select a role to add:',
-    choices: ['Engineer', 'Intern', 'Manager'],
-    }
-
-]
-
-const engineerQuestions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is engineer's name?"
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "What is engineer's ID?"
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is engineer's email?"
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is engineer's GitHub?"
-    },
-    
-
-]
-
-
-
-const managerQuestions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is manager's name?"
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "What is manager's ID?"
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is manager's email?"
-    },
-    {
-        type: 'input',
-        name: 'officeNumber',
-        message: "What is manager's office number?"
-    }
+  {
+    type: "list",
+    name: "choice",
+    message: "Select a role to add:",
+    choices: ["Engineer", "Intern", "Manager"],
+  },
 ];
 
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is engineer's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is engineer's ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is engineer's email?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is engineer's GitHub?",
+  },
+];
 
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is manager's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is manager's ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is manager's email?",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is manager's office number?",
+  },
+];
 
 const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the intern's name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the intern's ID?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the intern's email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "Which school did intern attend?",
+  },
+];
 
-]
-
-
-
-
-exports.managerQuestions = managerQuestions;
-exports.engineerQuestions = engineerQuestions;
-exports.internQuestions = internQuestions;
 exports.role = role;
+exports.engineerQuestions = engineerQuestions;
+exports.managerQuestions = managerQuestions;
+exports.internQuestions = internQuestions;
+
