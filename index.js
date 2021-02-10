5fe8af0db4cbe9a6b2e797e03389eeb7d2feaff4
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-09 23:10:37 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const inquirer = require ('inquirer');
-const fs = require ('fs');
+const inquirer = require("inquirer");
 
-const Employee = require ('./lib/employee');
-const Engineer = require ('./lib/engineer');
-const Intern = require ('./lib/intern');
-const Manager = require ('./lib/manager');
+const fs = require("fs");
 
-const questions = require ('./questions')
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
 
-
+const questions = require("./questions");
