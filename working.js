de97b20bdefe4baa5b714ca04152c82599f3e9fd
Erum Dhukka <erumdhukka531@gmail.com>
2021-02-14 22:37:36 -0600

    trying to get role questions to populate from terminal

--- working.js
+++ working.js
@@ -8,8 +8,9 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const path = require("path");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.profile"); //need "team.profile" to generate and call later
+//Acttivity example from class
+const output = path.resolve(__dirname, "output");
+const outputPath = path.join(output, "team.profile"); //need "team.profile" to generate and call later
 
 const teamProfile = [];
 
